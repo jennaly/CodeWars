aa85cbc296a040fcfceab050a3eba1d499b9316b
jennaly <jenna@wicot.io>
2022-05-28 19:31:52 -0700

    add another challenge

--- 5-28-22.js
+++ 5-28-22.js
@@ -11,6 +11,36 @@ function sumTwoSmallestNumbers(numbers) {
     let sum = sorted[0] + sorted[1];
     return sum
 }
-  
-  
-  
+
+// Rock Paper Scissors
+
+// Let's play! You have to return which player won! In case of a draw return Draw!.
+
+// Examples:
+
+// rps('scissors','paper') // Player 1 won!
+// rps('scissors','rock') // Player 2 won!
+// rps('paper','paper') // Draw!
+ 
+// My solution
+// const rps = (p1, p2) => {
+//     if ((p1 === 'rock' && p2 === 'scissors') || (p1 === 'paper' && p2 === 'rock') || (p1 === 'scissors' && p2 === 'paper')) {
+//       return 'Player 1 won!'
+//     } else if ((p2 === 'rock' && p1 === 'scissors') || (p2 === 'paper' && p1 === 'rock') || (p2 === 'scissors' && p1 === 'paper')) {
+//       return 'Player 2 won!'
+//     } else {
+//       return 'Draw!'
+//     }
+//   };
+
+// Best solution
+const rps = (p1, p2) => {
+    if (p1 === p2) return "Draw!";
+    var rules = {rock: "scissors", paper: "rock", scissors: "paper"};
+    if (p2 === rules[p1]) {
+      return "Player 1 won!";
+    }
+    else {
+      return "Player 2 won!";
+    }
+  };
