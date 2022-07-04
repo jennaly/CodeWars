b927ad1b39a9945d4cf9ec727137e73bd698aa40
jennaly <jenna@wicot.io>
2022-07-04 12:27:56 -0700

    7/4/22 init

---
+++ 7-04-22.js
@@ -0,0 +1,11 @@
+class Kata {
+    static getVolumeOfCuboid(length, width, height) {
+      return length * width * height
+    }
+  }
+
+//params: length, width, and height 
+//questions: number type? will one measurement ever be 0?
+//returns: product of the three measurements
+//example: (2,3,) => 6
+//pseudo: multiply the three params and return the product
