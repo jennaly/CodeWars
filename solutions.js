7f27addd85b1c09ba226108f8063ec98746ee7d3
jennaly <jenna@wicot.io>
2022-07-07 11:53:23 -0700

    add kata

--- solutions.js
+++ solutions.js
@@ -23,4 +23,24 @@ function correctTail(bod, tail) {
   //test each pair against conditions: if a > 55 AND b > 7 => "senior", if not => "open"
   //push result of each pair into new array
   
-  
+//   Summation
+
+// Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+
+//param: a number (n)
+//returns: the summation of every number for one to n
+//example:summation(8) -> 36
+// 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8
+//pseudo: 
+//create a variable (output) with value = 0
+//loop thru numbers up to n
+//add each number to the output
+//return the output
+
+var summation = function (num) {
+  let output = 0;
+  for (let i = 1; i <= num; i++) {
+    output += i
+  }
+  return output
+}
