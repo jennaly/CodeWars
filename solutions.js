8fae12eb2732cd71474e1e4c48abe9ce20153fd9
jennaly <jenna@wicot.io>
2022-08-07 09:07:32 -0700

    add kata

--- solutions.js
+++ solutions.js
@@ -212,3 +212,16 @@ String.prototype.isUpperCase = function() {
 function boolToWord (bool) {
   return bool ? "Yes" : "No"
 }
+
+//   This function takes two numbers as parameters, the first number being the coefficient, and the second number being the exponent.
+// Your function should multiply the two numbers, and then subtract 1 from the exponent. Then, it has to print out an expression (like 28x^7). "^1" should not be truncated when exponent = 2.
+
+function derive(coefficient,exponent) {
+  return `${coefficient * exponent}x^${exponent - 1}`
+}
+
+//params: two numbers - coefficient and exponent
+//returns: product of the two numbers +"x^" + exponent - 1
+//questions: data type of arguments? negative exponents?
+//pseudo: basic math operations, use template literals to string everything together
+
