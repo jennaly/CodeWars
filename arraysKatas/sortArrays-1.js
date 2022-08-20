010f2c86fca9f1bc48bf690e9eaba991b01f4e25
jennaly <jenna@wicot.io>
2022-08-20 12:42:05 -0700

    add kata

---
+++ arraysKatas/sortArrays-1.js
@@ -0,0 +1,12 @@
+// Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.
+// given an array of elements
+// return the array with elements sorted in lexicographical order
+// question: need to validate that elements are strings?
+// pseudo: use .sort and return the array
+
+// input: names - unsorted array
+// output: sorted array
+sortme = function( names ){
+    return names.sort()
+    
+  }
