2d76be571671535a5160399687159901ab1f57a8
jennaly <jenna@wicot.io>
2022-08-23 20:57:04 -0700

    add strings kata

---
+++ stringsKatas.js/8-23-22.js
@@ -0,0 +1,19 @@
+// 7 kyu
+// Convert an array of strings to array of numbers
+// Some really funny web dev gave you a sequence of numbers from his API response as an sequence of strings!
+// You need to cast the whole array to the correct type.
+// Create the function that takes as a parameter a sequence of numbers represented as strings and outputs a sequence of numbers.
+// ie:["1", "2", "3"] to [1, 2, 3]
+// Note that you can receive floats as well.
+
+//PREP
+//params: array of numbers in string type
+//returns: array of numbers in number type
+//questions: empty string? 
+//ex: ["445", "6", "800"] => [445, 6, 800]
+//pseudo:
+//map elems, for each elem, use Number()
+
+function toNumberArray(stringarray){
+    return stringarray.map(num => Number(num))
+  }
