dfb5f48c2f057fdadaaf615079f9e888d52dde87
jennaly <106183040+jennaly@users.noreply.github.com>
2022-10-05 17:13:42 -0700

    add kata

--- stringsKatas/solutions.js
+++ stringsKatas/solutions.js
@@ -54,7 +54,6 @@ function strCount(obj){
   return count
 }
 
- 
 
 //given an object with properties and properties values that may contain strings
 //return the number of string values found in the object
@@ -74,3 +73,20 @@ function strCount(obj){
 //check if the element is a string
 //if it is, increment the counter
 //return the counter value
+
+//MULTIPLY THE STRINGS IN THE ARRAY
+// You received an array with two strings. Create a function that will return their product as a string. E.g.
+// arrMultiply(['9','6']) should return '54'
+
+//given an array with two integers in the form of strings
+//return the product
+//question: is the product returned as an integer or a string?
+//what to return in the case of an empty array?
+//will the array.length always be 2? will there ever be more than 2 elements?
+//pseudo:
+//use Number() to convert the strings into numbers
+//use toString() to convert the output into a string
+
+function arrMultiply(arr){
+  return (Number(arr[0]) * Number(arr[1])).toString()
+}
