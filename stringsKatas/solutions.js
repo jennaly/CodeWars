d119a4b096e406f960722f44f94eed8539066ed1
jennaly <106183040+jennaly@users.noreply.github.com>
2022-10-05 17:43:39 -0700

    add kata

--- stringsKatas/solutions.js
+++ stringsKatas/solutions.js
@@ -90,3 +90,22 @@ function strCount(obj){
 function arrMultiply(arr){
   return (Number(arr[0]) * Number(arr[1])).toString()
 }
+
+// Create a function to lowercase all strings in an array. Non-string items should remain unchanged.
+
+// Example
+// arrayLowerCase(['Red', 'Green']) == ['red', 'green']
+// arrayLowerCase([1, 'Green']) == [1, 'green']
+
+//given an array
+//return the array with elements that are strings lowercased
+//question: 
+//will there every be an empty array?
+//return every letter in the word lowercased? or just the first letter in the word?
+//return a new array or mutate the original?
+//pseudo:
+//iterate through elements in the array, check the type, if the element is a string, call .toLowerCase()
+
+function arrayLowerCase(arr) {
+  return arr.map(elem => typeof elem === "string" ? elem.toLowerCase() : elem)
+}
