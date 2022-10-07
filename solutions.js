a873839aced4bd58ae56c74a8d23ac5109581faa
jennaly <jenna@wicot.io>
2022-10-07 10:16:39 -0700

    add kata

--- solutions.js
+++ solutions.js
@@ -282,3 +282,22 @@ function paperwork(n, m) {
 //return number of pages needed (classmates x pages for paperwork)
 //pseudo
 //check if n < 0 or m < 0, if yes return 0, if not return n * m
+
+
+// Messi goals function
+
+// Messi is a soccer player with goals in three leagues:
+
+// LaLiga
+// Copa del Rey
+// Champions
+// Complete the function to return his total number of goals in all three leagues.
+
+// Note: the input will always be valid.
+
+// For example:
+
+// 5, 10, 2  -->  17
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals
+}
