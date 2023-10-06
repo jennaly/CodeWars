a7941b51264b36c0f4df8ddf61d38b62988b109f
jennaly <jenna@wicot.io>
2023-10-06 12:16:00 -0700

    add optimized solution

--- middleNode.js
+++ middleNode.js
@@ -39,3 +39,29 @@ class LinkedList {
 
 // time complexity of O(n) * 2 => O(n) where n is the number of nodes iterated through
 // space comlexity of O(1), not storing anything extra
+
+function middleNode(linkedList) {
+  // Write your code here.
+  let slowNode = linkedList;
+  let fastNode = linkedList;
+
+  while (fastNode !== null && fastNode.next !== null) {
+    slowNode = slowNode.next;
+    fastNode = fastNode.next.next;
+  }
+  
+  return slowNode;
+}
+
+// take advantage of the linked list being sorted
+// use a fast pointer and a slow pointer
+// the fast pointer will travel by 2 nodes at a time
+// the slow pointer travels by a node at a time
+// when the fast pointer reaches the end of the list, the slow pointer will be halfway through the list
+// return the slow node
+
+// in the even length case, the loop stops when the fast pointer is null
+// in the odd length case, the loop stops when the fastponiter.next is null
+
+// time complexity of O(n) where n is the number of nodes
+// space complexity of O(1)
